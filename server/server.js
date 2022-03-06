259b4faf99250c4bd6fa081bd761a3aad1c1fd99
Marcos Bazbih <marcosbaza100@gmail.com>
2022-03-06 19:35:29 +0200

    100% done

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 require("./DB");
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5050;
-const cors = require("cors");
 const animalsRoutes = require("./routes/animals-routes");
 const employeesRoutes = require("./routes/employees-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -19,7 +19,6 @@ app.use(cors());
 app.listen(port, () => { console.log(`connected to port ${port}`) });
 
 app.get("/", (req, res) => res.send("<h1>Welcome to my zoo</h1>"))
-
 
 app.use(passport.initialize());
 app.use("/animals", passport.authenticate('jwt', { session: false }), animalsRoutes);
